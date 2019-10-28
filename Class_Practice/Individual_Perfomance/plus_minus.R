6752a79d5817948518c079a92dc4a5be6a3f03ec
narek12345 <nareksahakyan4@gmail.com>
2019-10-28 15:45:31 +0400

    Plus Minus(Ind Perd)

--- Class_Practice/Individual_Perfomance/plus_minus.R
+++ Class_Practice/Individual_Perfomance/plus_minus.R
@@ -1,38 +1,44 @@
+load("nba5.rda")
 load("nba12.rda")
 load("nba12p.rda")
-load("nba5.rda")
-library(ggplot2)
 library(dplyr)
+library(ggplot2)
 
+nba5 <- data.frame(PM = nba12$PointsScoredHome - nba12$PointsScoredAway, nba5)
+model <- lm(PM ~ ., data = nba5)
+coeff <- sort(coefficients(model), decreasing = T)
+coeff[1:10]
 
-nba3 <-nba12[, c("HomePlayer1ID","HomePlayer2ID",
-                 "HomePlayer3ID","HomePlayer4ID","HomePlayer5ID")]
-nba4 <-nba12[, c("AwayPlayer1ID","AwayPlayer2ID",
-                 "AwayPlayer3ID","AwayPlayer4ID","AwayPlayer5ID")]
+name1 <- names(coeff)
+name1 <- gsub("X", "", name1)
+name1 <- as.data.frame(name1)
 
+nba12p$PlayerID <- as.character(nba12p$PlayerID)
+name2 <- left_join(name1, nba12p[,1:2], by = c("name1" = "PlayerID"))
 
+names(coef) <- name2$PlayerTrueName
+coeff[1:10]
 
+ggplot(data = nba12p, aes(SimpleMin)) + 
+  geom_histogram() + 
+  ggtitle("Histogram for Played Minutes")
 
-# Make ID as character
-nba3 <- as.data.frame(sapply(nba3, as.character))
-nba4 <- as.data.frame(sapply(nba4, as.character))
-#create dummy variables
-nba3 <- as.data.frame(model.matrix(~. - 1, nba3))
-nba4 <- as.data.frame(model.matrix(~. - 1, nba4))
-# for away team
-nba4[nba4==1]<- -1
-# change colnames to be more readable
-colnames(nba3) <- gsub("HomePlayer[123456789]ID","", colnames(nba3))
-colnames(nba4) <- gsub("AwayPlayer[123456789]ID","", colnames(nba4))
-# nba3 remove double entries
-nba3 <- t(nba3) # transpose the data.frame
-nba3 <- by(nba3,INDICES=row.names(nba3),FUN=colSums) # collapse the rows with the same name
-nba3 <- as.data.frame(do.call(cbind,aggr))
-nba4 <- t(nba4) # transpose the data.frame
-nba4 <- by(nba4,INDICES=row.names(nba4),FUN=colSums) # collapse the rows with the same name
-nba4 <- as.data.frame(do.call(cbind,nba4))
-#combine home and away players and double columns
-nba5<-cbind(nba3,nba4)
-nba5 <- t(nba5) # transpose the data.frame
-nba5 <- by(nba5,INDICES=row.names(nba5),FUN=colSums) # collapse the rows with the same name
-nba5 <- as.data.frame(do.call(cbind,nba5))
+more250 <- nba12p$PlayerID[nba12p$SimpleMin > 250]
+length(more250)
+
+colnames(nba5) <- gsub("X","",colnames(nba5)) 
+nba6 <- nba5[,colnames(nba5) %in% more250]
+nba6 <- data.frame(PM = nba5$PM, nba6)
+
+model2 <- lm(PM ~ .,data = nba6)
+coeff2 <- sort(coefficients(model2), decreasing = T)
+coeff2[1:10]
+
+name3 <- names(coeff2)
+name3 <- gsub("X", "", name3)
+name3 <- as.data.frame(name3)
+name4 <- left_join(name3, nba12p[,1:2], by = c("name3" = "PlayerID"))
+
+names(coeff2) <- name4$PlayerTrueName
+coeff2[1:10]
+
