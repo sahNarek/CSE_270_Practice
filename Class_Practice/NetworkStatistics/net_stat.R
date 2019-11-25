22d30a78ada2fa20760a38c4247a624d3d34d94b
narek12345 <nareksahakyan4@gmail.com>
2019-11-26 01:10:12 +0400

    feat:(net_stats, HW5)

--- Class_Practice/NetworkStatistics/net_stat.R
+++ Class_Practice/NetworkStatistics/net_stat.R
@@ -72,3 +72,55 @@ ggplot() + geom_histogram(aes(x = avg_pl)) +
 
 mean(avg_pl < mean_distance(Moreno_i, directed = F))
 
+plot(net_i)
+E(net_i)
+
+head_of(net_i, E(net_i))
+table(head_of(net_i, E(net_i)))
+
+tail_of(net_i, E(net_i))
+table(tail_of(net_i, E(net_i)))
+
+net_i["A","B"]
+net_i["A",c("B","C")]
+
+incident(net_i, "A", mode = c("out"))
+incident(net_i, "A", mode = c("in"))
+
+neighbors(net_i, "A", mode = c("all"))
+neighbors(net_i, "C", mode = c("in"))
+
+A <- neighbors(net_i, "A", mode = c("out"))
+D <- neighbors(net_i, "D", mode = c("in"))
+
+intersection(A,D)
+
+farthest_vertices(net_i)
+
+get_diameter(net_i)
+
+ego(net_i, 2, "A", mode = c("out"))
+ego(net_i, 2, "D", mode = c("in"))
+
+net %v% "vertex.names"
+sna::degree(net)
+
+plot(net, displaylabels = T)
+
+net %v% "vertex.names"
+sna::degree(net, cmode = "indegree")
+sna::degree(net, cmode = "outdegree")
+
+sna::closeness(net, gmode = "graph")
+plot(net, displaylabels = T)
+
+igraph::closeness(net_i, mode = "all", normalized = T)
+
+network::set.edge.attribute(net, "weight", c(30,10,5,2,6))
+igraph::closeness(asIgraph(net), normalized = T)
+
+network::set.edge.attribute(net, "weight", 1/c(30,10,5,2,6))
+igraph::closeness(asIgraph(net), normalized = T)
+
+igraph::betweenness(net_i)
+sna::betweenness(net)
